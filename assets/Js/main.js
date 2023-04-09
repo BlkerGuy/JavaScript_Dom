6b3eb1b8e0cadbd828cd11e8ebc2748442c8839f
BlkerGuy <2020.gafarov@gmail.com>
2023-04-09 20:07:13 +0400

    bug-fixed

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -4,27 +4,27 @@ let right = document.querySelector('#right');
 let left = document.querySelector('#left');
 let count = 1;
 
-
-
-
-
-
-
-// (count==1)? add[0].classList.toggle('active'):'';
 right.addEventListener('click', RightSideSwitch);
 left.addEventListener('click', LeftSideSwitch);
 
-if (document.getElementById("slider")) {
-    count = 0;
-    const slider = () => {
-        document.querySelector("#slider").classList.add('fade-in');
+//Slider auto 
+// if (document.getElementById("slider")) {
+//     count = 0;
+//     const slider = () => {
+//         document.querySelector("#slider").classList.add('fade-in');
 
-        RightSideSwitch()
-    };
-    slider(); // Start slider immediately
-    setInterval(slider, 3000); // Slide every 4 seconds
-}
+//         RightSideSwitch()
+//     };
+//     slider(); // Start slider immediately
+//     setInterval(slider, 3000); // Slide every 4 seconds
+// }
+
+//Slide next img
+add[0].classList.toggle('active');
+
 function RightSideSwitch() {
+    // (count<0)? count++:'';  
+    debugger;
     for (let i = count; i < add.length;) {
         add[i].classList.toggle('active');
         ((i != 0) ? i-- : i = 2);
@@ -38,8 +38,8 @@ function RightSideSwitch() {
         break;
     }
 }
+//Slide back img
 function LeftSideSwitch() {
-
     (count == 2) ? count -= 3 : '';
 
     for (let i = ++count; i < add.length;) {
@@ -51,10 +51,10 @@ function LeftSideSwitch() {
             break;
         }
         count++
-        (count == 2) ? count = -1 : '';
         break;
     };
 }
+
 
 
 
