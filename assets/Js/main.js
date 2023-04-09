77ee663a61f28421e2c3a8f081383f699ef789c3
BlkerGuy <2020.gafarov@gmail.com>
2023-04-09 20:51:58 +0400

    slide-update/bug-fixing-code-optimized

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -1,42 +1,36 @@
 "use strict";
+
 let add = document.querySelectorAll('.card-size img');
 let right = document.querySelector('#right');
 let left = document.querySelector('#left');
 let count = 1;
 
+
+// Customize Time: default 4 sec
+let setSlideSpeedTime=0;
+
 right.addEventListener('click', RightSideSwitch);
 left.addEventListener('click', LeftSideSwitch);
+add[0].classList.remove('active');
 
-//Slider auto 
-// if (document.getElementById("slider")) {
-//     count = 0;
-//     const slider = () => {
-//         document.querySelector("#slider").classList.add('fade-in');
 
-//         RightSideSwitch()
-//     };
-//     slider(); // Start slider immediately
-//     setInterval(slider, 3000); // Slide every 4 seconds
-// }
 
 //Slide next img
-add[0].classList.toggle('active');
-
 function RightSideSwitch() {
-    // (count<0)? count++:'';  
-    debugger;
-    for (let i = count; i < add.length;) {
-        add[i].classList.toggle('active');
-        ((i != 0) ? i-- : i = 2);
-        add[i].classList.add('active');
-        add[1].style.transition = "all 1000s";
-        if (count == add.length - 1) {
-            count = 0;
+add[0].classList.add('active');
+
+        for (let i = count; i < add.length;) {
+            add[i].classList.toggle('active');
+            ((i != 0) ? i-- :(i==0)? i=2 :''); 
+            add[i].classList.add('active');
+            if (count == add.length - 1) {
+                count = 0;
+                break;
+            }
+            count++;
             break;
         }
-        count++;
-        break;
-    }
+    
 }
 //Slide back img
 function LeftSideSwitch() {
@@ -55,6 +49,14 @@ function LeftSideSwitch() {
     };
 }
 
+// Slider auto 
+if (document.querySelector('#slider')) {
+    const slider = () => {
+        document.querySelector("#slider").classList.add('fade-in');
+        setInterval(RightSideSwitch,(setSlideSpeedTime!=0)? setSlideSpeedTime:4000);
+    };
+    slider();
+}
 
 
 
